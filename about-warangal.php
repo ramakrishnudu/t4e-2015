ba9be85e25911ec818d101e82a007ab1c14d893d
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-10 17:40:22 +0530

    Create about-warangal.php

---
+++ about-warangal.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>About Hosting City</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                        <!-- Sub Section -->
+                        <div class="box-content"> 
+                            <!-- Box Content -->
+                            <p><h3 class="blog-title">Closeby Attractions</h3></p>
+
+                            <!--Contect for Area of focus-->
+                            <p>Waraed in Telangana state, 140kms from north–east of Hyderabad .Warangal, an ancient city, was once the capital of the Kakatiya kingdom which ruled over much of the state from the 12th to the early 14th centuries. The district has some interesting historic remnants and a number of temples built in ornate architectural style.</p>
+
+                               <!-- Section 1 -->
+
+                            <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                    <h2> Ramappa Temple</h2>
+                                    <p>The Ramappa Temple is situated at Palampet, about 77 kms. from Warrangal. This temple represents a combination of Chalukya and Hoysala art of architecture. It is one of the finest examples of temple architecture in South India.</p>
+                                </div>
+                            </div><!-- End of Section 1 -->
+
+                            <!-- Section 2 -->
+
+                            <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                    <h2>Warangal Fort</h2>
+                                    <p>Warangal Fort, in the present-day Indian state of Telangana, appears to have existed since at least the 13th century CE. Although precise dating of its construction and subsequent enhancements are uncertain, historians and archaeologists generally accept that an earlier brick-walled structure was replaced with stone by Ganapatideva, who died in 1262, and that his successors – Rudramadevi and Prataparudra II – added to its height and added gateways, square bastions and additional circular earthern walls prior to the latter's death in 1323.</p>
+                                </div>
+                            </div><!-- End of Section 2 -->
+                            
+                            <!-- Section 3 -->
+
+                            <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                <h2>Thousand Pillar Temple</h2>
+                                <p>The Thousand Pillar Temple is situated on the slopes of the Hananakonda Hill. This temple was built by Rudra Deva in 1163 AD in the style of Chalukyan temples, star shaped and triple shrined and a fine specimen of Kakatiya architecture and sculpture. There are three shrines which are dedicated to Lord Shiva, Vishnu and Surya. The temple is known for its richly carved pillars, screens and detailed sculpture. The black basalt Nandi, a monolith, has a lovely polished finish.</p>
+                                 </div>
+                            </div><!-- End of Section 3 -->
+
+
+                            <!-- Section 4 -->
+
+                            <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                <h2>The Bhadrakali Temple</h2>
+                                <p>Dedicated to the Goddess 'Bhadrakali' is a precious deity of this region. Located on the hilltop between the twin cities of Hanamkonda and Warangal with a artificial lake of 2 ½ kms radius is one of the major attractions of the city. The main attraction of the temple is the square image of the goddess (2.7 X 2.7 meters), an ancient Bhadrakali in sitting posture wearing a crown with eight hands, holding weapons. The surrounding mountain area of the temple is developed into a sacred one with about 8 major and 12 minor temples attracting lot number of devotees.</p>
+                                 </div>
+                            </div><!-- End of Section 4 -->
+
+
+                            <!-- Section 4 -->
+
+                            <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                <h2>The Pakhal Lake And The Wild Life Sanctuary</h2>
+                                <p>About 50 kms east of the historical city of Warangal, amidst undulating forest-clad hills and dales is the man made lake Pakhal built during 12-13 AD in the reign of King Ganapathi Deva. It stands as a live example of the Vastu Vignana (Knowledge of Vastu), which found its expression in the building activity of the Kakatiya kings. Around the shores of this lake is the Pakhal Wild Life Sanctuary covering an area of 900 Sq. Kms with dense forest, Shelter of tigers and a large variety of protected animals.</p>
+                                </div>
+                            </div>
+
+                        </div> <!-- End of Box Content -->
+                    </div> <!-- End of Sub Section -->
+                </div> <!-- End of Main Content Section -->
+
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
