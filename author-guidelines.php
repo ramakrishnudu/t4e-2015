c2548f659a3f1a019dfee0988c038c07a5666575
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 18:58:08 +0530

    Update author-guidelines.php

--- author-guidelines.php
+++ author-guidelines.php
@@ -1,1 +1,94 @@
-#
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+
+        <!-- Experiment with dd -->
+        <link rel="stylesheet"
+  href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+<link rel="stylesheet"
+  href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Author Guidelines</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                        <!-- Sub Section -->
+                        <div class="box-content"> 
+                            <!-- Box Content -->
+                            
+                            <p>Authors are required to follow <a href="http://www.ieee.org/conferences_events/conferences/publishing/templates.html">IEEE Conference Publishing Services Formatting Guidelines.</a> </p>
+                            <p>Authors should submit papers using the <a href="https://easychair.org/conferences/?conf=t4e2015">EasyChair Conference Management System.</a></p>
+                            <p>Submitted papers will be subject to a peer review process. Authors need to submit complete papers for the review process, abstract alone will not be sufficient.</p>
+                            <p>The conference employs a double-blind review process, hence authors' names and affiliations should not be present within the manuscript at the time of submission. Authors should include their names only in the appropriate fields in the paper submission form on EasyChair. Authors' information should be included ONLY in the camera-ready version of the manuscript and NOT in the initial version.</p>
+                            <p><b>Note:</b> Please clear your cache and cookies if you get "incorrect page" error while redirecting to easychair submission page.</p>
+
+                            
+                        </div>
+                    </div>
+                </div> <!-- /.row -->
+
+                        
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->       
+
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
+
