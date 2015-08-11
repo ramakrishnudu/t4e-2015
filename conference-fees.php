0a1b9793fbda1da487ea0cad4467b6bb4ba95f37
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:11:53 +0530

    Update conference-fees.php

--- conference-fees.php
+++ conference-fees.php
@@ -1,1 +1,211 @@
-# conference-fees
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+        http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Conference Fees</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                    <!-- Sub Section -->
+                        <div class="box-content"> 
+                        <!-- Box Content -->
+                        <p>At least one unique author of each accepted paper must register by 13 October 2015 in order for the paper to be included in the proceedings. Student registrations are only acceptable if the first author of the paper is a full-time student at an accredited institution. </p>
+                        <p>Please note that, according to the recently adopted IEEE Conferences No Show Policy, we should notify you that "IEEE reserves the right to exclude a paper from distribution after the conference (e.g., removal from IEEEXplore) if the paper is not presented at the conference". Therefore, please take into serious consideration this notification when planning your participation to the T4E 2015. Conference.</p>
+
+
+                    <!-- Begining of Table 2 Block -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row">
+                                <div class="col-xs-3" style="background-color:#f9f9f9; font-weight:700">Membership category</div>    
+                                <div class="col-xs-2" style="background-color:#f9f9f9; font-weight:700">Advance Registration </div>
+                                <div class="col-xs-2" style="background-color:#f9f9f9; font-weight:700">Late/onsite Registration</div>                
+                                </div>
+                            </div>
+                        </div>    
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row">
+                                    <div class="col-xs-3"></div>
+                                    <div class="col-xs-1 add-left-padding add-color">US $ </div>
+                                    <div class="col-xs-1 remove-left-padding add-color">Rs.</div>
+                                    <div class="col-xs-1 add-left-padding add-color">US $</div>
+                                    <div class="col-xs-1 remove-left-padding add-color">Rs.</div>
+                                </div>
+                            </div>
+                        </div> 
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row">
+                                    <div class="col-xs-3">IEEE Member</div>
+                                    <div class="col-xs-1 add-left-padding">142 </div>
+                                    <div class="col-xs-1 remove-left-padding">9,230</div>
+                                    <div class="col-xs-1 add-left-padding">175</div>
+                                    <div class="col-xs-1 remove-left-padding">11,375</div>
+                                </div>
+                            </div>
+                        </div>       
+
+
+                            <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row">
+                                    <div class="col-xs-3">Non IEEE Member</div>
+                                    <div class="col-xs-1 add-left-padding">207 </div>
+                                    <div class="col-xs-1 remove-left-padding">13,455</div>
+                                    <div class="col-xs-1 add-left-padding">265</div>
+                                    <div class="col-xs-1 remove-left-padding">17,225</div>
+                                </div>
+                            </div>
+                        </div>     
+
+                            <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row">
+                                    <div class="col-xs-3">IEEE Student Member</div>
+                                    <div class="col-xs-1 add-left-padding">73 </div>
+                                    <div class="col-xs-1 remove-left-padding">4,745</div>
+                                    <div class="col-xs-1 add-left-padding">93</div>
+                                    <div class="col-xs-1 remove-left-padding">6,045</div>
+                                </div>
+                            </div>
+                        </div>
+
+                            <div class="row">
+                            <div class="col-xs-12">
+                                <div class="row style-col-row">
+                                    <div class="col-xs-3">Non IEEE Student Member</div>
+                                    <div class="col-xs-1 add-left-padding">95</div>
+                                    <div class="col-xs-1 remove-left-padding">6,175</div>
+                                    <div class="col-xs-1 add-left-padding">120</div>
+                                    <div class="col-xs-1 remove-left-padding">7,800</div>
+                                </div>
+                            </div>
+                        </div>                               
+                        </div>        
+                        <!-- Ending of Table 2 Block -->
+<!--                         <div class="panel panel-default remove-only-margin">
+                            <div class="panel-body">
+                                <h2>Payment using Bank Transfer</h2>
+                                <p>&nbsp;</p>
+                                <p>Account name:  T4E2015</p>
+                                <p>Account number: 62405632123</p>
+                                <p>Bank:  State Bank of Hyderabad</p>
+                                <p>Branch:  NIT Branch, warangal</p>
+                                <p>IFSC Code:  SBHY0020149</p>
+                                <p>Swift Code:  SBHYINBB018</p>                                    
+
+                            </div>
+                        </div> -->
+                        <p>The registration fee includes full entry to all events (Keynotes/Invited Speeches, Workshops, Papers Presentations, Poster/Demos Sessions and Panels), lunch and conference dinner as well as one electronic copy of the T4E 2013 Proceedings</p>
+                        <p>All supporting documents (such as bonafide student certificate, IEEE membership card, Payment proof etc. as applicable) should be sent as soft copy through email <a>t4e2015nitwregn@nitw.ac.in</a>  and hard copy along with the registration form through Speed Post to:</p>
+
+                        <div class="panel panel-default remove-only-margin">
+                            <div class="panel-body">
+                                <h2>Prof . G. Amba Prasad Rao</h2>
+                                
+                                <p>T4E2015 Registration Chair</p>
+                                <p>Department of Mechanical Engineering</p>
+                                <p>National Institute of technology</p>
+                                <p>Warangal, Telangana – 506004</p>
+                                <p>Email: t4e2015nitwregn@nitw.ac.in</p>
+                                <p>Mobile: 91 8332969375</p>
+                                <p>Land Line: 91 870 2462342</p>
+                            </div>
+                        </div>
+
+                        <h2>Mode of Payment:</h2>
+                        <p>Indian participants can register for the conference by sending their payments as Demand Drafts or through online bank transfer.</p>
+                        <div class="panel panel-default remove-only-margin">
+                                <div class="panel-body">
+                                    <h2>a)  Demand Draft: </h2>
+                                    <p>The demand draft should be drawn in favor of T4E2015 payable at State Bank of Hyderabad, NIT branch, Warangal. Registration ID and name of the participant must be mentioned on the reverse of the DD. </p>
+                                    <p>&nbsp;</p>
+                                    <h2>b) Direct Payment / Third-Party Transfer:</h2>
+                                    <p>Account name:  T4E2015</p>
+                                    <p>Account number: 62405632123</p>
+                                    <p>Bank:  State Bank of Hyderabad</p>
+                                    <p>Branch:  NIT Branch, warangal</p>
+                                    <p>IFSC Code:  SBHY0020149</p>
+                                    <p>Swift Code:  SBHYINBB018</p>
+
+                               </div>
+                        </div>
+                        <h2>Participants who will register from outside India</h2>
+                        <p>Participants who will register from outside India are requested to either send a Bank Draft in US$ or do a wire transfer or pay online using their credit card. The online payment mechanism using credit card will be updated soon.</p>
+                                     
+
+
+
+
+
+
+
+
+                        </div> <!-- End of Box Content -->
+                    </div> <!-- End of Sub Content Section -->
+                </div> <!-- End of Main Content Section -->
+                
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
