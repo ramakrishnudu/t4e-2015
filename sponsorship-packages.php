3ac01db4f56047a4c4b74f7d7eeafa0689c41f7c
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:21:50 +0530

    Update sponsorship-packages.php

--- sponsorship-packages.php
+++ sponsorship-packages.php
@@ -1,1 +1,256 @@
-sponsorship-packages
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+                <!-- Experiment with dd -->
+        <link rel="stylesheet"
+          href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+        <link rel="stylesheet"
+          href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?>
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Sponsorship Package</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+                <div class="project-detail row">
+                    <!-- <div class="project-slider col-md-12">
+                        <img src="images/projects/big_project_1.jpg" alt="Slide 1">
+                        <img src="images/projects/big_project_2.jpg" alt="Slide 2">
+                        <img src="images/projects/big_project_3.jpg" alt="Slide 1">
+                        <a href="#" class="slidesjs-previous slidesjs-navigation">&lt;</a> 
+                        <a href="#" class="slidesjs-next slidesjs-navigation">&gt;</a>
+                    </div> --> <!-- /.project-slider -->
+
+                    <div class="project-infos col-md-12">
+                        <div class="box-content">
+                            <p>T4E 2015 is happy to offer three conference packages. The platinum, gold, and silver sponsorship packages allow conference participants to gain maximum brand exposure at the T4E 2015 conference events.</p>
+                            <p>The sponsorship packages offer sponsors a number of branding opportunities. Some of these benefits include recognition as event sponsor, exhibition booths at the conference venue, logo on the workshop bags, and bag inserts among others. </p>
+                            <h2 class="project-title">Event Sponsorship Packages & Entitlements</h2>
+
+                            <!-- Section 1 -->
+                            <div class="row add-top-margin">
+                                <div class="section-header2 col-md-12"><h2> Platinum, Gold, Silver Sponsorship</h2>
+                                    <p>Below are the sponsorship packages and entitlements. For more information on sponsorship,pricing, or advertising, please email t4e2015@gmail.com.</p>
+                                </div>
+                            </div><!-- End of Section 1 -->
+
+                            <!-- Begining of table block -->
+                            <!-- This is the table -->
+                            <table class="table table-striped">
+                              <thead>
+<!-- 
+                            <table class="table table-striped">
+                                <thead>
+ -->                                    <tr>
+                                        <th>Sponsorship Packages & Entitlements</th>
+                                        <th>Platinum</th>
+                                        <th>Gold</th>
+                                        <th>Silver</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Sponsorship fee</td>
+                                        <td>INR 5 lacs & above
+                            (USD 8000& above )</td>
+                                        <td>INR 2 lacs to 5 lacs
+                            (Between USD 3000
+                            & 8000)</td>
+                                        <td>INR 1 lac to 2 lacs
+                            (USD 1500 to 3000)</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Exhibition Booths
+                            with display tables,
+                            wi-fi, and electrical
+                            points.</td>
+                                        <td>10 ft x 10 ft x 8 ft</td>
+                                        <td>10 ft x 8 ft x 8 ft</td>
+                                        <td>10 ft x 6 ft x 8 ft</td>
+                                    </tr>
+                                    <tr>
+                                        <td>TV Screen/25 inches
+                            monitor</td>
+                                        <td>Yes</td>
+                                        <td>Yes</td>
+                                        <td>No</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Complimentary
+                            delegate registration</td>
+                                        <td>Conference
+                            registration fee
+                            waiver for 8
+                            delegates</td>
+                                        <td>Conference
+                            registration fee
+                            waiver for 4 delegate</td>
+                                        <td>Conference
+                            registration fee
+                            waiver for 2 delegate</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Recognition as Event
+                            Sponsor</td>
+                                        <td><ol><li>Logo on T4E
+                            main website</li><li>Logo in
+                            conference
+                            brochure & marketing
+                            material</li></ol></td>
+                                        <td><ol><li>Logo listed on
+                            T4E sponsors
+                            page in the
+                            website</li><li>Logo in conference
+                            brochures &
+                            marketing
+                            material</li></ol></td>
+                                        <td>Logo listed on T4E
+                            sponsors page in the
+                            website</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Bag insert (USB
+                            and/or flier)</td>
+                                        <td>Yes</td>
+                                        <td>Yes</td>
+                                        <td>No</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Name/logo on
+                            signage at on-site
+                            registration desk</td>
+                                        <td>Yes</td>
+                                        <td>No</td>
+                                        <td>No</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Invitation to chair a
+                            session at the
+                            conference (a panel
+                            discussion or
+                            moderate the industry
+                            track paper
+                            presentations)</td>
+                                        <td>Yes</td>
+                                        <td>Yes</td>
+                                        <td>No</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div></div>
+
+                            <!-- Section 2 -->
+                            <div class="row add-top-margin">
+                                <div class="section-header2 col-md-12"><h2> Other Sponsorship Categories</h2>
+
+                                <h3>Category A: You could sponsor any or all of the following. Please select.</h3>
+                                <ul class="fa-ul">
+                                    
+                                    <li><i class="fa-li fa fa-square-o icon-align"></i>Production of registration packs (Rs. 40,000)</li>
+                                    <li><i class="fa-li fa fa-square-o icon-align"></i>Cost of icebreaker reception (Rs. 35,000)</li>
+                                    <li><i class="fa-li fa fa-square-o icon-align"></i>Printing of abstract volume (Rs. 30,000)</li>
+                                    <li><i class="fa-li fa fa-square-o icon-align"></i>Poster board hire (Rs. 40,000)</li>
+                                    <li><i class="fa-li fa fa-square-o icon-align"></i>Hosting expenses for keynote speakers (Rs. 40,000 each)</li>
+
+                                </ul>
+
+                                <h3>What you get?</h3>
+                                <ul class="fa-ul">
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>Space for your display stand (including kit) </li>
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>Your logo in conference publicity such as abstract volume, correspondence, conference literature, and conference audiovisual system.</li>
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>50% discount in conference registration for 1 delegate</li>
+                                </ul><br/>
+
+                                    <h3>Category B: You could sponsor the following:</h3>
+
+                                    <ol class="ol-style">
+                                      <li>
+                                        <p>Lecture room hire and audiovisual equipment (Rs. 10,000)</p>
+                                      </li>
+                                      <li>
+                                        <p>Drinks for poster session (Rs. 15,000)</p>
+                                      </li>
+                                      <li>
+                                        <p>Contribution to conference dinner (Rs. 15,000)</p>
+                                      </li>
+                                      <li>Local transport (Rs. 10,000)</li>
+                                      <li>Any other contribution specifying how you would like it to be used</li>
+                                    </ol>                                                       
+                                <h3>What you get?</h3>
+                                <ul class="fa-ul">
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>Space for your display stand (including kit)</li>
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>Your logo in conference publicity such as abstract volume, correspondence, conference literature, and conference audiovisual system.</li>
+                                    <li><i class="fa-li fa fa-circle icon-align"></i>50% discount in conference registration for 1 delegate</li>
+                                </ul><br/>
+
+                                <p>We expect about 200 delegates at the conference. Many of these will be faculty, educational institutions, researchers, future purchasers of equipment and software such as yours.</p><br/>
+                                <p>If you would like more information about the conference, please visit http://www.ask4research.info/t4e/2015/ or alternatively contact Conference Sponsorship Chair
+                                Prof NVSN Sarma, NIT Warangal, India at sarma@nitw.ac.in or +91 9849639262.</p><br/>
+                                <h2 class="project-title">Sponsorship Form</h2><br/>
+                                <p><!-- Please fill the sponsorship form below: --> You may fill the sponsorship form <a href="pdf/sponsorship-packages.pdf" target="_blank">online</a> or <a href="pdf/sponsorship-packages.pdf" download="sponsorship-packages.pdf">download</a> the copy of the form and mail it to </p>
+                                <hr>
+                                <ul></ul>
+                            </div>
+                        </div>
+
+
+                        </div> <!-- /.box-content -->
+                    </div> <!-- /.project-infos -->
+                </div> <!-- /.project-detail -->
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
