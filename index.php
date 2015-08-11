0468ef3e4dc6cc9263cc796e5ae7bc847695831d
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,113 @@
-index
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+        <div class="swiper-container" >
+            <div class="swiper-wrapper">
+        
+
+                <div class="swiper-slide" > <!-- style="background-image: url(images/T4E_warangal2015_website3b_1100.png);" -->
+                    <div class="overlay-s"></div>
+                    <div class="slider-caption">
+                        <div class="inner-content">
+                            <p class="first">The 7<sup class="sup-first">th</sup> IEEE International Conference on</p>
+                            <p class="second">Technology for Education (T4E 2015)</p>   
+                            <p class="date">Dec 10-12, 2015<!-- <a rel="nofollow" href="http://unsplash.com">Unsplash</a> for photos. --></p>
+                            <p class="place">&nbsp;@ NIT Warangal, India</p>
+                            <p class="main-btn">Paper Submission Deadline (Extended): <span>&nbsp;15<sup class="sup-second">th</sup> July 2015</span></p>
+                            <!-- <p class="first" style="font-family: Arial">The 7<sup>th</sup> IEEE International Conference on </p>
+                            <p class="second">Technology for Education</h1>   
+                            <p class="date">Dec 10-12, 2015</p>
+                            <p class="place">NIT Warangal, India</p>
+                            <p href="#" class="main-btn">Paper Submission Deadline: <span class="last-date">&nbsp;1st July 2015</span></p> -->
+                        </div> <!-- /.inner-content -->
+                    </div>  <!-- /.slider-caption -->
+                 </div><!-- /.swier-slide -->
+
+                <!-- <div class="swiper-slide" style="background-image: url(images/T4E_warangal2015_website3b_1100.png);">
+                    <div class="overlay-s"></div>
+                    <div class="slider-caption">
+                        <div class="inner-content">
+                            <h2>The 7th IEEE International Conference on Technology for Education</h2>   
+                            <p>Dec 10-12, <b class="blue">2015</b><!-- <a rel="nofollow" href="http://unsplash.com">Unsplash</a> for photos. </p>
+                            <p><b class="green">NIT Warangal,</b> India</p>
+                            <p href="#" class="main-btn white">Paper Submission Deadline <span style="color:red; font-weight:900">&nbsp;1st July 2015</span></p>
+                        </div> --> <!-- /.inner-content -->
+                    <!-- </div> --> <!-- /.slider-caption -->
+                <!-- </div> --> <!-- /.swier-slide -->
+
+                <!-- <div class="swiper-slide" style="background-image: url(images/slide2.jpg);">
+                    <div class="overlay-s"></div>
+                    <div class="slider-caption">
+                        <div class="inner-content">
+                            <h2>Hotel and Residence Concept in Montenegro</h2>
+                            <p>We come with new fresh and unique ideas.</p>
+                            <a href="#" class="main-btn white">View Projects</a>
+                        </div> --> <!-- /.inner-content -->
+                    <!-- </div> --> <!-- /.slider-caption -->
+                <!-- </div> --> <!-- /.swier-slide -->
+
+                <!-- <div class="swiper-slide" style="background-image: url(images/slide3.jpg);">
+                    <div class="overlay-s"></div>
+                    <div class="slider-caption">
+                        <div class="inner-content">
+                            <h2>Natural 3d Architecture Design</h2>
+                            <p>Natural concrete is a material which is calm and clean.</p>
+                            <a href="#" class="main-btn white">View Projects</a>
+                        </div> --> <!-- /.inner-content -->
+                    <!-- </div> --> <!-- /.slider-caption -->
+                <!-- </div> --> <!-- /.swier-slide -->
+
+            </div> <!-- /.swiper-wrapper -->
+        </div> <!-- /.swiper-container -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+        
+    </body>
+</html>
