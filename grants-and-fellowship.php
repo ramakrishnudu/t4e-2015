0bfe94117df87f88decf58b16d3a0240210a7f1b
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:15:38 +0530

    Update grants-and-fellowship.php

--- grants-and-fellowship.php
+++ grants-and-fellowship.php
@@ -1,1 +1,85 @@
-grants-and-fellowship
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?><!-- /.site-header -->
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Grants and Fellowship</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                    <!-- Sub Section -->
+                        <div class="box-content"> 
+                        <!-- Box Content -->
+
+
+                                    <p><h3>IEEE Grants for Students with Accepted Papers in T4E 2015</h3><p>
+                                    <p><h3>Last date for application - September 5, 2015 </h3></p>
+                                    <p><h3>Winner Announcement - September 20, 2015</h3></p>
+                                    <p class="add-margin">IEEE Technical Committee on Learning Technology is pleased to provide several travel grants to stuents with accepted papers in T4E 2015. This grant fund can only be used to support a part of the airfare and/or accommodations for up to US$ 500 (maximum)/per student.</p>
+                                    <p class="add-margin">If you wish t to be considered for the grant, please send an email application by 10 September 2015 to t4e2015@gmail.com with subject of the email as "Candidate for IEEE TCLT-sponsored student travel grants at T4E 2015". Please provide the submission ID and the title of your paper in the email.</p>
+                                    <p class="add-margin">The grants will be directly reimbursed by IEEE after the conference. Winners will be asked to complete certain forms and provide scanned copies of receipts for final verification. It will be a claim-based reimbursement. They will need to submit the original copies of their receipts to the local organizing committee to claim reimbursement. Multiple claims will not be entertained.</p>
+
+                                
+                        </div> <!-- End of Box Content -->
+                    </div> <!-- End of Sub Content Section -->
+                </div> <!-- End of Main Content Section -->
+                
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
+
