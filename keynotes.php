dacdf3318de461980421d80198591283aaaa0f65
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:18:41 +0530

    Update keynotes.php

--- keynotes.php
+++ keynotes.php
@@ -1,1 +1,178 @@
-keynotes
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+                <style type="text/css">
+            a {
+    color: #0254EB
+}
+a:visited {
+    color: #0254EB
+}
+
+/*.comment {
+    width: 400px;
+    background-color: #f0f0f0;
+    margin: 10px;
+}*/
+a.morelink {
+    text-decoration:none;
+    outline: none;
+}
+.morecontent span {
+    display: none;
+
+}
+        </style>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Keynote Speakers</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                    <!-- Sub Section -->
+                        <div class="box-content"> 
+                        <!-- Box Content -->
+                        <p>&nbsp;</p>
+
+                        <div class="row">
+                            <div class="col col-md-12 col-sm-12 col-xs-12 text-center center-row" id="about_content">
+                                <div class="row center">
+                                      <div class="col col-md-4 col-sm-12 templatemo_ceo">
+                                        <img src="images/Nian-Shing-Chen.jpg" alt="Nian Shing Chen" class="center-block img-circle img-responsive">
+                                        <!-- <p>Linda, CEO</p> -->
+                                       <a href="http://scis.athabascau.ca/scis/staff/showStaffInfo.php?ct=nian-shing.chen" target="_blank"><p align="center" style="color:#f69730">Professor Nian Shing Chen</p></a>
+                                        <p style="text-align:center; margin: 2px 0">Department of Information Management</p>
+                                        <p style="text-align:center; margin: 2px 0">National Sun Yat-sen University, Taiwan</p>
+                                      </div>
+                                      <div class="col col-md-8 col-sm-12"><p align="justify"><b>Professor Nian Shing Chen</b> is Chair Professor at the Department of Information Management in the National Sun Yat-sen University, Taiwan. He has published over 400 papers in the international referred journals, conferences and book chapters. One of his papers published in Innovations in Education and Teaching International was awarded as the top cited article in 2010. He is the author of three books with one textbook entitled “e-Learning Theory & Practice”. <span class="name"><a href="#" id="example3-show" style="color:#f69730" onclick="showHide('example3');return false;">More...</a></span></p>
+<div id="example3" style="display:none;">
+      <p align="justify">Prof. Chen received the outstanding research awards from the National Science Council, Taiwan in 2008 and 2011-2013. His current research interests include assessing e-Learning course performance; online synchronous teaching & learning; mobile & ubiquitous learning; Natural user interface & Game-based learning. Prof. Chen is serving as an editorial board member for many international journals and guest editors for more than 12 special issues of international journals. He is Editor-In-Chief of the SSCI indexed Journal of Educational Technology & Society. He has also organized and chaired numerous international conferences and workshops in the area of advanced learning technologies.</p>
+      <p align="justify">Professor Chen is a senior member of IEEE, ACM and the Chair for Expert Seminars and Web-based Tutorials, the IEEE Technical Committee on Learning Technology (http://lttf.ieee.org/). Prof Chen has served at the Executive Board of the IEEE Technical Committee on Learning Technology for more than 15 years and he has served as General Co-Chair and Programme Co-Chair of TCLT’s flagship conference, ICALT. He is now the Chair of the IEEE Technical Committee on Learning Technology since 2012.</p> 
+      <span class="name"><a href="#" id="example3-hide" style="color:#f69730" onclick="showHide('example3');return false;">Less</a></span>
+   </div>
+   <h3 style="padding:10px 10px 0 10px; text-align:left"> <span style="color:blue">Talk Title: </span>Embodied Cognition and Gesture-based Learning</h3>
+   <p align="justify">Gesture-based computing has recently received great attention in educational technology. Increasing studies have attempted to integrate its major features—gestures and body motions—into learning activities ranging from physical-related domain to cognitive domain. Despite the rise of gesture-based computing, studies in psychology have long shown that gestures influence the way we learn, think and perceive the world, suggesting learning can be less sedentary. <span class="name"><a href="#" id="example4-show" style="color:#f69730" onclick="showHide('example4');return false;">More...</a></span></p>
+<div id="example4" style="display:none;">
+      <p align="justify">Theories of embodied cognition have been the basis to explain the effect of body on our mind. Thus, this talk attempts to explore the relationship between these theories and technologies by presenting: (1) the overview of the findings from psychology and education and the implementation of gestures/body motions in our own study cases, and (2) potential research frameworks for future studies. It is hoped that this talk could promote more researchers to do study on making learning kinesthetic and designing pedagogically meaningful gesture-based learning systems.</p> 
+      <span class="name"><a href="#" style="color:#f69730" id="example4-hide" onclick="showHide('example4');return false;">Less</a></span>     
+</div>        
+                                      </div>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                    
+                      <p>&nbsp;</p>
+
+                        <div class="row">
+                            <div class="col col-md-12 col-sm-12 col-xs-12 text-center center-row" id="about_content">
+                                <div class="row center">
+                                      <div class="col col-md-4 col-sm-12 templatemo_ceo">
+                                        <img src="images/Maiga-Chang.jpg" alt="Maiga Chang" class="center-block img-circle img-responsive">
+                                        <!-- <p>Linda, CEO</p> -->
+                                       <a href="http://maiga.athabascau.ca/" target="_blank"><p align="center" style="color:#f69730">Dr. Maiga Chang</p></a>
+                                        <p style="text-align:center; margin: 2px 0">School of Computing and Information Systems</p>
+                                        <p style="text-align:center; margin: 2px 0">Athabasca University, Canada</p>
+                                      </div>
+                                      <div class="col col-md-8 col-sm-12">
+                                       <p align="justify"><b>Dr. Maiga Chang</b> is Associate Professor in the School of Computing Information and Systems at Athabasca University, Canada. His researches mainly focus on mobile learning and ubiquitous learning, museum e-learning, game-based learning, educational robots, learning behaviour analysis, data mining, intelligent agent technology, computational intelligence in e-learning, and mobile healthcare. Dr. Chang is co-chair of Asia-Pacific Region Game and Toy Enhanced Learning and Society special interest group (SIG) under the Asia-Pacific Society for Computers in Education (APSCE). He is now editor in chief of International Journal of Distance Education Technologies. He has been guest edited 13 special issues for many international journals. <span class="name"><a href="#" id="list-show" style="color:#f69730" onclick="showHide('list');return false;">More...</a></span></p>
+<div id="list" style="display:none;">
+      <p align="justify">Beside serving hundreds of international conferences and workshops as a program committee member, Dr. Chang is also track program chair of Digital Game and Intelligent Toy Enhanced Learning in IEEE International Conference on Advanced Learning Technologies 2014 and 2015, PC co-chair of ICCE sub-conference on Digital Game and Digital Toy Enhanced Learning and Society (GTEL&S) from 2013 to 2015, advisory board member of IADIS International Conference on Mobile, Hybrid, and On-line Learning (2012~now), special area chair in pervasive education of International Conference on Systems and Networks Communications (2011~now), program co-chair of Edutainment 2011, general co-chair of Edutainment 2009, and local chair of IEEE DIGITEL 2008.</p>
+      <p align="justify">Dr. Chang has given 66 talks and lectures in different conferences, universities, and events; he also has (co-)authored more than 185 edited books, book chapters, journal and international conference papers. He is an IEEE member since 1996 and also a member of ACM (since 2001), AAAI (since 2001), INNS (since 2004), and Phi Tau Phi Scholastic Honor Society.</p> 
+      <span class="name"><a href="#" id="list-hide" style="color:#f69730" onclick="showHide('list');return false;">Less</a></span>
+   </div>
+   <h3 style="padding:10px 10px 0 10px; text-align:left"> <span style="color:blue">Talk Title: </span>Context-aware Mobile Educational Game</h3>
+   <p align="justify">The trend of designing and developing mobile apps attracts many researchers’ attention on using mobile devices to make users do learning activities in authentic environments such as museums and historical sites. Some other researchers further develop mobile games for educational purpose; these games further make users get motivated via the game-play experience. My research team proposed and designed a context-aware educational game-based mobile application. <span class="name"><a href="#" id="list1-show" style="color:#f69730" onclick="showHide('list1');return false;">More...</a></span></p>
+<div id="list1" style="display:none;">
+      <p align="justify">The game can generate inquiry-based learning activities for users according to their needs (under informal learning situation) and the courses they are taking (under formal learning situation). This talk will reveal the thoughts and itineray of conducting mobile educational game research, explain pilots and the methods were taken to analyze the collected data, and share important findings and lessons we learned.</p> 
+      <span class="name"><a href="#" style="color:#f69730" id="list1-hide" onclick="showHide('list1');return false;">Less</a></span>     
+</div>        
+                                      
+
+                                      </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                                
+                        </div> <!-- End of Box Content -->
+                    </div> <!-- End of Sub Content Section -->
+                </div> <!-- End of Main Content Section -->
+                
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+                
+            })
+            //]]>
+        </script>
+
+        </script>
+            <script language="javascript" type="text/javascript">
+function showHide(shID) {
+   if (document.getElementById(shID)) {
+      if (document.getElementById(shID+'-show').style.display != 'none') {
+         document.getElementById(shID+'-show').style.display = 'none';
+         document.getElementById(shID).style.display = 'block';
+      }
+      else {
+         document.getElementById(shID+'-show').style.display = 'inline';
+         document.getElementById(shID).style.display = 'none';
+      }
+   }
+}
+</script>
+
+    </body>
+</html>
