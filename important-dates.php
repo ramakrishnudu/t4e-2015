ef87185d455de6145545c306121af23c7ee44201
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:17:19 +0530

    Update important-dates.php

--- important-dates.php
+++ important-dates.php
@@ -1,1 +1,132 @@
-important-dates
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>T4E2015 - IEEE Conference on technology for education</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,300italic,300,500,500italic,700,900">
+
+        <!-- Experiment with dd -->
+        <link rel="stylesheet"
+  href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+<link rel="stylesheet"
+  href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+        <!--
+        Artcore Template
+		http://www.templatemo.com/preview/templatemo_423_artcore
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <section id="pageloader">
+            <div class="loader-item fa fa-spin colored-border"></div>
+        </section> <!-- /#pageloader -->
+
+        <?php include_once("header.php");?> <!-- /.site-header -->
+
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Important Dates</h2>
+                        <!-- <span>Subtitle Goes Here</span> -->
+                    </div> <!-- /.section-header -->
+                </div> <!-- /.row -->
+
+                <div class="project-detail row">
+                <!-- Main Content Section -->
+                    <div class="project-infos col-md-12"> 
+                        <!-- Sub Section -->
+                        <div class="box-content"> 
+                            <!-- Box Content -->
+
+                                <!-- Begining of table block -->
+                        <div class="table-responsive">
+
+                           <table class="table table-striped">
+                              <thead>
+                                 <tr>
+                                    <th><strike>Paper submission deadline</strike></th>
+                                    <th><strike>1 July 2015</strike></th>
+                                 </tr>
+                              </thead>
+                              <tbody>
+                                 <tr>
+                                     <td><strike>Paper submission deadline</strike></td>
+                                     <td><strike>15 July 2015</strike></td>
+                                 </tr>
+                                 <tr>
+                                     <td><strike>Paper final submission deadline<strike></td>
+                                     <td><strike>22 July 2015</strike></td>
+
+                                 </tr>
+                                 <tr>
+                                    <td>Notification of paper acceptance</td>
+                                    <td>7 September 2015</td>
+                                    
+                                 </tr>
+                                 <tr>
+                                    <td>Camera-ready paper submission</td>
+                                    <td>23 September 2015</td>
+                                    
+                                 </tr>
+                                 <tr>
+                                    <td>Author registration</td>
+                                    <td>13 October 2015</td>
+                                    
+                                 </tr>
+                                 <tr>
+                                    <td>Early Bird registration</td>
+                                    <td>3 November 2015</td>
+                                    
+                                 </tr>
+                                 <tr>
+                                    <td>Conference dates</td>
+                                    <td>10-12 December 2015</td>
+                                    
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </div> 
+                        <!-- End of table block -->     
+                        </div>
+                    </div>
+                </div>    
+
+            </div> <!-- /.inner-content -->
+        </div> <!-- /.content-wrapper -->
+
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Preloader -->
+        <script type="text/javascript">
+            //<![CDATA[
+            $(window).load(function() { // makes sure the whole site is loaded
+                $('.loader-item').fadeOut(); // will first fade out the loading animation
+                    $('#pageloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+                $('body').delay(350).css({'overflow-y':'visible'});
+            })
+            //]]>
+        </script>
+
+    </body>
+</html>
