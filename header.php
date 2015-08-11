836830d0fd3c31ddfb76a52accb2297af6834c05
ramakrishnudu <ramakrishnudu.t@gmail.com>
2015-08-11 19:16:47 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,135 @@
+<header class="site-header container-fluid">
+            <div class="top-header">
+                <div class="logo col-md-6 col-sm-6 col-xs-4 ">
+                        <img src="home/T4E_logo.png" class="img-responsive" />
+                        <span>Knowledge, skill and competency for all!</span>    
+                </div>
+                <!-- <div class="logo col-md-6 col-sm-6">
+                    <h1><a href="index.html"><em>T4E</em>2015</a></h1>
+                    <span>Knowledge, skill and competency for all!</span>
+                </div> --> <!-- /.logo -->
+                <div class="social-top col-md-6 col-sm-6">
+                    <ul>
+                        <li><a href="https://www.facebook.com/pages/T4E-2015/881929558539908?ref=aymt_homepage_panel" target="_blank" class="fa fa-facebook"></a></li>
+                        <li><a href="https://twitter.com/T4E2015" target="_blank" class="fa fa-twitter"></a></li>
+                 <!--        <li><a href="#" class="fa fa-linkedin"></a></li>
+                        <li><a href="#" class="fa fa-google-plus"></a></li>
+                        <li><a href="#" class="fa fa-flickr"></a></li>
+                        <li><a href="#" class="fa fa-rss"></a></li> -->
+                    </ul>
+                </div> <!-- /.social-top -->
+            </div> <!-- /.top-header -->
+            <div class="main-header">
+                <div class="row">
+                    <div class="main-header-left col-md-3 col-sm-6 col-xs-8">
+                        <!-- <a id="search-icon" class="btn-left fa fa-search" href="#search-overlay"></a>
+                        <div id="search-overlay">
+                            <a href="#search-overlay" class="close-search"><i class="fa fa-times-circle"></i></a>
+                            <div class="search-form-holder">
+                                <h2>Type keywords and hit enter</h2>
+                                <form id="search-form" action="#">
+                                    <input type="search" name="s" placeholder="" autocomplete="off" />
+                                </form>
+                            </div>
+                        </div> --><!-- #search-overlay -->
+                        <!-- <a href="#" class="btn-left arrow-left fa fa-angle-left"></a>
+                        <a href="#" class="btn-left arrow-right fa fa-angle-right"></a> -->
+                    </div> <!-- /.main-header-left -->
+                    <div class="menu-wrapper col-md-9 col-sm-6 col-xs-4">
+                        <a href="javascript:void(0);" class="toggle-menu visible-sm visible-xs"><i class="fa fa-bars"></i></a>
+                        <ul class="sf-menu hidden-xs hidden-sm">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="javascript:void(0);">Call for Papers</a>
+                                <ul>
+                                    <li><a href="call-for-paper.php">Call for Papers</a></li>
+                                    <li><a href="author-guidelines.php">Author Guidelines</a></li>
+                                    <li><a href="important-dates.php">Important Dates</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="javascript:void(0);">Registration</a>
+                                <ul>
+                                    <li><a href="conference-fees.php">Conference Fees</a></li>
+                                    <li><a href="grants-and-fellowship.php">Grants & Fellowship</a></li>
+									<li><a href="javascript:void(0)" onclick="alertUser();">Registration</a></li>
+<!--                                     <li><a href="project-details.html">Project Single</a></li> -->
+                                </ul>
+                            </li>
+                            <li><a href="javascript:void(0);">Program</a>
+                                <ul>
+                                    <li><a href="program-schedule.php">Program Schedule</a></li>
+                                    <li><a href="keynotes.php">Keynotes</a></li>
+                                    <li><a href="panels.php">Panel</a></li>
+                                    <li><a href="workshops.php">Workshops</a></li>
+<!--                                     <li><a href="tutorials.html">Tutorials</a></li> -->
+                                    <li><a href="contact-chair.php">Contact Program Chair</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="javascript:void(0);">Conference</a>
+                                <ul>
+                                    <li><a href="date-and-venue.php">Date & Venue</a></li>
+                                    <li><a href="chair-and-committee.php">Chairs/ Committees</a></li>
+                                    <li><a href="about-warangal.php">About Warangal</a></li>
+                                    <li><a href="accomodation.php">Accommodation</a></li>
+                                    <li><a href="sponsorship-packages.php">Sponsors</a></li>
+                                </ul>
+                            </li>                                        
+
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div> <!-- /.menu-wrapper -->
+                </div> <!-- /.row -->
+            </div> <!-- /.main-header -->
+            <div id="responsive-menu">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li ><a href="javascript:void(0);">Call for Papers</a>
+                    <!-- <li><a href="#">Projects</a> -->
+                        <ul>
+                            <li><a href="call-for-paper.php">Call for Papers</a></li>
+                            <li><a href="author-guidelines.php">Author Guidelines</a></li>
+                            <li><a href="important-dates.php">Important Dates</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="javascript:void(0);">Registration</a>
+                        <ul>
+                            <li><a href="conference-fees.php">Conference Fees</a></li>
+                            <li><a href="grants-and-fellowship.php">Grants & Fellowship</a></li>
+							<li><a href="javascript:void(0)" onclick="alertUser();">Registration</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="javascript:void(0);">Program</a>
+                        <ul>
+                            <li><a href="program-schedule.php">Program Schedule</a></li>
+                            <li><a href="keynotes.php">Keynotes</a></li>
+                            <li><a href="panels.php">Panel</a></li>
+                            <li><a href="workshops.php">Workshops</a></li>
+                            <!-- <li><a href="tutorials.html">Tutorials</a></li> -->
+                            <li><a href="contact-chair.php">Contact Program Chair</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="javascript:void(0);">Conference</a>
+                                <ul>
+                                    <li><a href="date-and-venue.php">Date & Venue</a></li>
+                                    <li><a href="chair-and-committee.php">Chairs/ Committees</a></li>
+                                    <li><a href="about-warangal.php">About Warangal</a></li>
+                                    <li><a href="accomodation.php">Accommodation</a></li>
+                                    <li><a href="sponsorship-packages.php">Sponsors</a></li>
+                                </ul>
+                            </li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </header> 
+<script type="text/javascript">
+function alertUser()
+{
+	var r = confirm("This is page will redirected to secured T4E 2015 maintenance website http://www.bda2015.nitw.ac.in/t4e-2015/registration.php");
+	if (r == true) {
+		window.location.href = "http://www.bda2015.nitw.ac.in/t4e-2015/registration.php";
+		
+		return true;
+	} else {
+		return true;
+}
+}
+</script>		
