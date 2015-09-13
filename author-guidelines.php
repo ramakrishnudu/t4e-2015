35f0df951ccebab23d3df54eb7a3d42a708248e4
AmulyaSri <pamulyasri@gmail.com>
2015-09-13 21:18:17 +0530

    Updating the content given by Prof. Venkatesh

--- author-guidelines.php
+++ author-guidelines.php
@@ -56,57 +56,242 @@
                         <!-- Sub Section -->
                         <div class="box-content"> 
                             <!-- Box Content -->
-                            <div class="panel panel-default remove-only-margin">
-                                <div class="panel-body">
-                                    <p>Please follow the instructions given below for submission of your camera-ready paper and copyright form in IEEE format and compatible with IEEE Xplore.</p><br>
-									<h2 style="font-size:20px;"><b>1. General Instructions for camera ready paper and copyright form submission</b></h2>
-                                    <br>
-                                    <p>i. Prepare your camera ready paper by following section 2 and copyright form by following section 3. Please follow the following steps to ensure your paper is included in the T4E2015 conference proceedings published by IEEE Computer Society (and included in IEEE Xplore).</p>
-									<p>ii. The final paper(both in PDF and original file in doc/odt/latex) and copyright form should be zipped and to be submitted to EasyChair by SEPTEMBER 23<sup>rd</sup>, 2015. The instructions for submitting final paper in EasyChair are given in section 4.</p>
-									<p>iii. Atleast one author has to register by October 13<sup>th</sup>, 2015, for the paper to be published in conference proceeding. For registration following the registration link in T4E website.</p>
-									 <br><h2 style="font-size:20px;"><b>2. Instructions for camera ready paper</b></h2>
-									 <br>
-									 <p>i. Please ensure the page limit of your paper complies with the category in which your paper has been accepted (which may be different from the category in which you submitted the paper).</p>
-									 <p>ii. The final camera-ready version of your paper should incorporate all reviewer comments. The reviewers' comments can be found at by logging on to your Easychair account <a href="http://www.easychair.org/" target="_blank">http://www.easychair.org/</a> homepage. </p>
-									 <p>iii. No page numbers should be used in final camera ready paper.</p>
-									 <p>iv. Final paper in PDF should comply with IEEE format for conference paper submission. Authors should obtain a clean compliance report from IEEE PDF Express by following the steps given below: </p>
-									 <p>a)Go directly to Step 5.3 in  <a href="http://www.ieeeconfpublishing.org/cpir/authorKit.asp?Facility=CPS_Dec&ERoom=T4E+2015" target="_blank"> Author Kit </a>  (Please ignore all other steps in the Author Kit)</p>
-									 <p>b) In this step, run your paper through PDF eXpress tool to check if your camera-ready manuscript is in IEEE format and compatible with IEEE Xplore. You will be required to create an account for which the conference ID to use is 37100X.</p>
-									 <p>c) After clearing the check, the PDF file has to be uploaded in EasyChair . DO NOT UPLOAD THE PAPER IN THE IEEE SITE. </p>
-									 <br><h2 style="font-size:20px;"><b>3. Copyright Form submission</b></h2>
-									 <br>
-									 <p>a) While in the IEEE site, you must also complete the Copyright Release Form in the Step 7 of the Author Kit</p>
-									 <p>b)You will receive an email confirming the submission of Copyright Release Form. This email has to be submitted in a Word or PDF file along with your final paper in EasyChair.</p>
-                                    <br>
-									<h2 style="font-size:20px;"><b>4. Final Submission in EasyChair</b></h2>
-									<br>
-									<p>After logging in to EasyChair </p>
-									<p>1. Select: "My submission"->submission #<br>
-									2. On your right side menu, select "proceedings version"-> "upload new version"<br>
-									3. Upload your zipped file containing 3 files:<br>
-									i. Camera ready paper in PDF and in original format(DOC, ODT,Latex)<br>
-									ii. Copyright form.</p>
-                                </div>
-								<br>
-							
-
-
-
-
-
-
-
-
+                           
+                           
+                           <div class="panel-body">
                             
+                                    <p>Please follow the
+                                    instructions given below
+                                    to submit your
+                                    camera-ready paper and
+                                    the IEEE copyright form
+                                    so that your paper is
+                                    compatible with the IEEE
+                                    publication format and
+                                    is included in the
+                                    conference
+                                    proceedings.</p>
+
+                                    <h2 style="font-size:20px;"><b>1. General
+                                    Instructions for
+                                    camera-ready paper and
+                                    copyright form
+                                    submission</b></h2>
+
+                                    <ul>
+                                      <li type="lower-latin"
+                                    style="margin-left:30px"> <p>
+					Prepare your
+					camera-ready paper
+					(Section 2 below)
+					and complete the
+					copyright form
+					(Section 3).</p></li>
+
+                                    <li type="lower-latin"
+                                    style="margin-left:30px"><p
+                                    The final paper (both in
+                                    PDF and the original
+                                    <code>doc/odt/latex</code>
+                                    files) and copyright
+                                    form should be zipped
+                                    and submitted to
+                                    <em>EasyChair</em>
+                                    by <strong>September 23,
+                                    2015</strong> (See
+                                    Section 4.)</p></li>
+
+                                    <li type="lower-latin"
+                                    style="margin-left:30px">
+                                    <p>At least one author
+                                    must register
+                                    by <strong>October 13,
+                                    2015</strong>, for the
+                                    paper to be published in
+                                    the conference
+                                    proceedings.  For
+                                    registration please
+                                    follow
+                                    the <a href="registration.html">registration
+                                    page</a> (will be open
+                                    soon).</p></li>
+				    </ul>
+				    
+				    <h2 style="font-size:20px;"><b>2. Instructions
+				    for camera-ready paper
+				    submission</b></h2>
+
+                                     <ul>
+				       <li  type="lower-latin"
+					    style="margin-left:30px"><p>
+					    Please ensure
+					    the page limit
+					    of your paper
+					    complies with
+					    the category in
+					    which your paper
+					    has been
+					    accepted.  Note
+					    that this may be
+					    different from
+					    the category for
+					    which you
+					    submitted the
+					    paper.</p></li>
+
+                                     <li type="lower-latin"
+                                     style="margin-left:30px"><p>
+                                     The final camera-ready
+                                     version of your paper
+                                     should incorporate all
+                                     reviewer comments.  You
+                                     may find the reviewers'
+                                     comments by logging on
+                                     to your Easychair
+                                     account <a href="http://www.easychair.org/"
+                                     target="_blank">http://www.easychair.org/</a>
+                                     homepage.</p></li>
+
+                                     <li type="lower-latin"
+                                     style="margin-left:30px">
+                                     <p>No page numbers should
+                                     appear in your final
+                                     camera-ready
+                                     version.</p></li>
+
+                                     <li type="lower-latin"
+                                     style="margin-left:30px"><p>
+                                     To ensure that the
+                                     final paper complies
+                                     with the IEEE format,
+                                     please follow the steps
+                                     outlined in
+                                     the <a href="http://www.ieeeconfpublishing.org/cpir/authorKit.asp?Facility=CPS_Dec&ERoom=T4E+2015"
+                                     target="_blank"> IEEE
+                                     Author Kit</a>.</p>
+
+                                     <ul>
+                                     <li type="lower-roman"
+                                     style="margin-left:30px"><p>Go
+                                     directly to <strong>Step 5.3</strong>
+                                     in  the Author
+                                     Kit and follow the
+                                     steps outlined there.</p></li>
+
+                                     <li type="lower-roman"
+                                     style="margin-left:30px"><p>
+                                     Run your paper through
+                                     the <a href="www.pdf-express.org"
+                                     target="blank"> PDF
+                                     eXpress</a> tool
+                                     mentioned in the
+                                     instructions to check
+                                     if your camera-ready
+                                     manuscript is in the
+                                     IEEE format.  You will
+                                     first need to create an
+                                     account using the
+                                     conference
+                                     ID <code>37100X</code>.</p></li>
+
+                                     <li type="lower-roman"
+                                     style="margin-left:30px"><p>
+                                     After clearing the
+                                     check, please upload
+                                     the PDF file to
+                                     EasyChair.  DO NOT
+                                     UPLOAD THE PAPER IN THE
+                                     IEEE SITE. </p></li>
+
+                                     </li>
+				     </ul>
+                                     <h2 style="font-size:20px;"><b>3. Copyright
+                                     form completion and acknowledgement</b></h2>
+
+                                     <ul><li type="lower-latin"
+                                     style="margin-left:30px"><p>
+                                     Please complete
+                                     the Copyright Release
+                                     Form in the Step 7 of
+                                     the <a href="http://www.ieeeconfpublishing.org/cpir/authorKit.asp?Facility=CPS_Dec&ERoom=T4E+2015"
+                                     target="_blank">Author
+                                     Kit</a>.<p></li>
+
+                                     <li type="lower-latin"
+                                     style="margin-left:30px"><p>
+                                     You will receive an
+                                     email confirming the
+                                     submission of Copyright
+                                     Release Form.  A PDF or
+                                     Word version of this
+                                     email will need to be
+                                     submitted along with
+                                     your final paper in
+                                     EasyChair (Step 4
+                                     below).<p></li>
+                                     </ul>
+                                    <br>
+                                    <h2 style="font-size:20px;"><b>4. Final
+                                    Submission in
+                                    EasyChair</b></h2>
+
+                                    <br>
+
+				    <p> Log in to
+				    <a href="http://www.easychair.org"
+				    target="blank">Easychair</a>.
+				    Then follow the
+				    steps below:</p>
+
+                                    <ul>
+
+				    <li type="lower-latin"
+                                    style="margin-left:30px">Select
+                                    "My
+                                    submission"->submission
+                                    #.</li>
+
+                                    <li type="lower-latin"
+                                    style="margin-left:30px">
+                                    Select "proceedings
+                                    version" from the menu
+                                    on the right.  Next,
+                                    select "upload new
+                                    version".</li>
+
+                                    <li type="lower-latin"
+                                    style="margin-left:30px">
+                                    Upload your zipped file
+                                    containing three things:
+
+                                    <ul>
+                                    <li type="lower-roman"
+                                    style="margin-left:30px">
+                                    Camera ready paper in
+                                    PDF.</li>
+ 				    <li type="lower-roman"
+                                    style="margin-left:30px">
+                                    Original source files of
+                                    the paper (DOC, ODT, or
+                                    LaTeX)</li>
+
+                                    <li type="lower-roman"
+                                    style="margin-left:30px">
+                                    Copyright form.</li>
+                                    </ul></li></ul>
+                                </div>
+                            <!-- <p>Authors are required to follow <a href="http://www.ieee.org/conferences_events/conferences/publishing/templates.html">IEEE Conference Publishing Services Formatting Guidelines.</a> </p>
+                            <p>Authors should submit papers using the <a href="https://easychair.org/conferences/?conf=t4e2015">EasyChair Conference Management System.</a></p>
+                            <p>Submitted papers will be subject to a peer review process. Authors need to submit complete papers for the review process, abstract alone will not be sufficient.</p>
+                            <p>The conference employs a double-blind review process, hence authors' names and affiliations should not be present within the manuscript at the time of submission. Authors should include their names only in the appropriate fields in the paper submission form on EasyChair. Authors' information should be included ONLY in the camera-ready version of the manuscript and NOT in the initial version.</p>
+                            <p><b>Note:</b> Please clear your cache and cookies if you get "incorrect page" error while redirecting to easychair submission page.</p> -->
                         </div>
                     </div>
                 </div> <!-- /.row -->
-
-                        
+                            
             </div> <!-- /.inner-content -->
-        </div> <!-- /.content-wrapper -->       
-
-
+        </div> <!-- /.content-wrapper --> 
+                           
         <script src="js/vendor/jquery-1.11.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
